c7b5649c9aadd3b51de019a73fa6ae9794194471
Karukan0814 <46077409+Karukan0814@users.noreply.github.com>
2024-02-12 21:12:40 -0800

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+// import { NavLink } from "react-router-dom";
+
+// import { logo } from "../assets/images";
+
+const Navbar = () => {
+  return (
+    <header className="header">
+      hedaer
+      {/* <NavLink to="/"> 
+      <p></p>
+       </NavLink> */}
+      {/* <nav className="flex text-lg gap-7 font-medium">
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive ? "text-blue-600" : "text-black"
+          }
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/projects"
+          className={({ isActive }) =>
+            isActive ? "text-blue-600" : "text-black"
+          }
+        >
+          Projects
+        </NavLink>
+      </nav> */}
+    </header>
+  );
+};
+
+export default Navbar;
